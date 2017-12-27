574efbb4def6894f0e16a930038bbefb6c162032
wzcxm <dark_zc@hotmail.com>
2017-12-27 17:03:58 +0800

    update

--- xxgameserice/routes/web.php
+++ xxgameserice/routes/web.php
@@ -17,7 +17,6 @@ $router->get('/', function () use ($router) {
 
 $router->get('/login/{uid}&{type}&{value}','GameLoginController@login');
 
-$router->get('/test', function () use ($router) {
-    return "holle lumen hahah";
-});
+
+
 
