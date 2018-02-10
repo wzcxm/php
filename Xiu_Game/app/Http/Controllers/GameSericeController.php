92466d3534d10008e5da0b82c78edc76e5eaf9e4
wzcxm <dark_zc@hotmail.com>
2018-02-10 11:33:06 +0800

    201802101133

--- Xiu_Game/app/Http/Controllers/GameSericeController.php
+++ Xiu_Game/app/Http/Controllers/GameSericeController.php
@@ -34,7 +34,7 @@ class GameSericeController extends Controller
             foreach ($data as $item){
                 $nownum += $item['level'];
                 if($rand <= $nownum){
-                    $this->setUserInfo($uid,$item['id']);
+                    $this->setUserInfo($uid,$item);
                     return $item['id'];
                 }
             }
@@ -43,80 +43,21 @@ class GameSericeController extends Controller
         }
     }
 
-    private function setUserInfo($uid,$type){
+    private function setUserInfo($uid,$item){
         try{
-            $name = "";
-            $upstype = "";
-            $num = 0;
-            switch ($type){
-                case 1:
-                    $name = '100金币';
-                    $upstype = 'coin';
-                    $num = 100;
-                    break;
-                case 2:
-                    $name = '300金币';
-                    $upstype = 'coin';
-                    $num = 300;
-                    break;
-                case 3:
-                    $name = '500金币';
-                    $upstype = 'coin';
-                    $num = 500;
-                    break;
-                case 4:
-                    $name = '1000金币';
-                    $upstype = 'coin';
-                    $num = 1000;
-                    break;
-                case 5:
-                    $name = '2钻石';
-                    $upstype = 'card';
-                    $num = 2;
-                    break;
-                case 6:
-                    $name = '8钻石';
-                    $upstype = 'card';
-                    $num = 8;
-                    break;
-                case 7:
-                    $name = '1元红包';
-                    $upstype = 'red';
-                    $num = 1;
-                    break;
-                case 8:
-                    $name = '2元红包';
-                    $upstype = 'red';
-                    $num = 2;
-                    break;
-                case 9:
-                    $name = '5元红包';
-                    $upstype = 'red';
-                    $num = 5;
-                    break;
-                case 10:
-                    $name = '1000元红包';
-                    $upstype = 'red';
-                    $num = 1000;
-                    break;
-            }
-            if($upstype == 'coin'){
-                DB::table('xx_user')->where('uid',$uid)->increment('gold', $num,['lottery' => 2]);
-                DB::table('xx_sys_prize')->insert(['name'=>$name,'uid'=>$uid,'code'=>$type,'type'=>1,'jptype'=>1]);
-                CommClass::UpGameSer($uid,'coin');//更新玩家金币
-            }else if($upstype == 'card'){
-                DB::table('xx_user')->where('uid',$uid)->increment('roomcard', $num,['lottery' => 2]);
-                DB::table('xx_sys_prize')->insert(['name'=>$name,'uid'=>$uid,'code'=>$type,'type'=>1,'jptype'=>2]);
-                CommClass::UpGameSer($uid,'card');//更新玩家金币
-            }else if($upstype == 'red'){
-                DB::table('xx_user')->where('uid',$uid)->increment('redbag', $num,['lottery' => 2]);
-                DB::table('xx_sys_prize')->insert(['name'=>$name,'uid'=>$uid,'code'=>$type,'type'=>1,'jptype'=>3]);
-                $user = Users::find($uid);
-                $message = "恭喜玩家：【".$user->nickname."】,在每日分享抽奖中，抽中【".$name."】奖品！";
-                CommClass::UpGameSer(1,'urgent',$message);
+            if($item['id'] !=1){
+                if($item['id'] == 9 || $item['id'] == 10){
+                    DB::table('xx_user')->where('uid',$uid)->update(['lottery' => 2]);
+                    DB::table('xx_sys_prize')->insert(['name'=>$item['name'],'uid'=>$uid,'code'=>$item['id'],'type'=>1,'jptype'=>1]);
+                }else{
+                    DB::table('xx_user')->where('uid',$uid)->increment('redbag', $item['value'],['lottery' => 2]);
+                    DB::table('xx_sys_prize')->insert(['name'=>$item['name'],'uid'=>$uid,'code'=>$item['id'],'type'=>1,'jptype'=>3]);
+                    $user = Users::find($uid);
+                    $message = "恭喜玩家：【".$user->nickname."】,在每日分享抽奖中，抽中【".$item['name']."】奖品！";
+                    CommClass::UpGameSer(1,'urgent',$message);
+                }
             }
         }catch (\Exception $e){
-            return "";
         }
     }
 
@@ -124,17 +65,17 @@ class GameSericeController extends Controller
     public function share($roomNo=0,$msg='')
     {
         try{
-//            $tools = new JsApiPay();
-//            $openid = $tools->GetOpenid();
-//            $unionid = $tools->data['unionid'];
-//            if(!empty($unionid)) {
-//                //下载人没有记录的保存记录
-//                $temp_user = DB::table('xx_user_temp')->where('unionid', $unionid)->first();
-//                if (empty($temp_user)) {
-//                    DB::table('xx_user_temp')->insert(['wxopenid' => $openid, 'unionid' => $unionid]);
-//                }
-//            }
-            $room =  null;//Redis::get('table_'.$roomNo);
+            $tools = new JsApiPay();
+            $openid = $tools->GetOpenid();
+            $unionid = $tools->data['unionid'];
+            if(!empty($unionid)) {
+                //下载人没有记录的保存记录
+                $temp_user = DB::table('xx_user_temp')->where('unionid', $unionid)->first();
+                if (empty($temp_user)) {
+                    DB::table('xx_user_temp')->insert(['wxopenid' => $openid, 'unionid' => $unionid]);
+                }
+            }
+            $room =  Redis::get('table_'.$roomNo);
             if(!empty($room))
             {
                 $ret_arr = [];
