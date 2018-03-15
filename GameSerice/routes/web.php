4e61eb3a09118d011a2610250d99b198fb0dd2f6
wzcxm <dark_zc@hotmail.com>
2018-03-15 09:31:57 +0800

    201803150931

--- GameSerice/routes/web.php
+++ GameSerice/routes/web.php
@@ -38,7 +38,7 @@ $router->get('/V/{version}/{type}','GameSericeController@GetVersion');
 //下载地址
 $router->get('/GetUrl',function (){
     return ['android'=>'https://pro-app-tc.fir.im/55ad19b179b374052dea4d83005d62ca11c49e1c.apk?sign=8b33da59c1d6fcb7925ab828131e9649&t=5aa9d16a',
-        'ios'=>'http://fir.im/4zn2',
+        'ios'=>'https://pro-app-tc.fir.im/55ad19b179b374052dea4d83005d62ca11c49e1c.apk?sign=8b33da59c1d6fcb7925ab828131e9649&t=5aa9d16a',
         'resources'=>'http://cspp-collection.oss-cn-shenzhen.aliyuncs.com/update_package/',
         'files'=>'AB,resources.ab,image.ab,lua.ab'] ;
 });
