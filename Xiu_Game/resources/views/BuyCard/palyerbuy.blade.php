0adc43c04e62a3d8fdbced2f175c3cd2d0add95d
wzcxm <dark_zc@hotmail.com>
2018-04-24 15:22:11 +0800

    201404241522

--- Xiu_Game/resources/views/BuyCard/palyerbuy.blade.php
+++ Xiu_Game/resources/views/BuyCard/palyerbuy.blade.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" href="{{asset('js/weui/css/weui.min.css')}}">
     <link rel="stylesheet" href="{{asset('js/weui/css/jquery-weui.min.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('/css/bootstrap.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('/css/style.css')}}?v=20180424">
+    <link rel="stylesheet" type="text/css" href="{{asset('/css/style.css')}}?v=201804241">
 </head>
 <body>
 <div class="buy_bg">
