58743adb40b446b30c4b2fbdfcf9b258c0e9a44e
wzcxm <dark_zc@hotmail.com>
2018-05-16 15:27:56 +0800

    201805161527

--- Xiu_Game/app/Http/Controllers/UpdateIdController.php
+++ Xiu_Game/app/Http/Controllers/UpdateIdController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Users;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -15,6 +16,10 @@ class UpdateIdController extends Controller
             $new_uid = isset($data['new_uid'])?$data['new_uid']:0;
             $old_teaid = isset($data['old_teaid'])?$data['old_teaid']:0;
             $new_teaid = isset($data['new_teaid'])?$data['new_teaid']:0;
+            $new_user = DB::table('xx_user')->where('uid',$new_uid)->get();
+            if(!empty($new_user) || count($new_user)>0){
+                return response()->json(['Error'=>'新id已存在！']);
+            }
             if(!empty($old_uid) && !empty($new_uid)){
                 DB::select('CALL update_uid('.$old_uid.','.$new_uid .')');
             }
