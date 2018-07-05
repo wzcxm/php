d17054052afd8f9c5451d7e3d9949fca7696f1f1
wzcxm <dark_zc@hotmail.com>
2018-07-05 18:42:06 +0800

    201807051842

--- Xiu_Game/config/conf.php
+++ Xiu_Game/config/conf.php
@@ -5,12 +5,12 @@ return [
             10001,10701,10071,12001,10101
         ],
         "Prize"=>[
-            ['id'=>2,'name'=>'5000金豆','value'=>5000,'level'=>10000,'type'=>'bean'],
-            ['id'=>7,'name'=>'0.88元红包','value'=>0.88,'level'=>5000,'type'=>'red'],
-            ['id'=>6,'name'=>'2钻石','value'=>2,'level'=>2000,'type'=>'diamond'],
-            ['id'=>4,'name'=>'5钻石','value'=>5,'level'=>1000,'type'=>'diamond'],
-            ['id'=>5,'name'=>'5.88元红包','value'=>5.88,'level'=>500,'type'=>'red'],
-            ['id'=>3,'name'=>'8.88元红包','value'=>8.88,'level'=>300,'type'=>'red'],
+            ['id'=>6,'name'=>'2钻石','value'=>2,'level'=>10000,'type'=>'diamond'],
+            ['id'=>7,'name'=>'0.88元红包','value'=>0.88,'level'=>8000,'type'=>'red'],
+            ['id'=>4,'name'=>'5钻石','value'=>5,'level'=>5000,'type'=>'diamond'],
+            ['id'=>2,'name'=>'5000金豆','value'=>5000,'level'=>2500,'type'=>'bean'],
+            ['id'=>5,'name'=>'5.88元红包','value'=>5.88,'level'=>1000,'type'=>'red'],
+            ['id'=>3,'name'=>'8.88元红包','value'=>8.88,'level'=>200,'type'=>'red'],
             ['id'=>1,'name'=>'IphoneX','value'=>0,'level'=>0,'type'=>'diamond'],
         ]
 ];
