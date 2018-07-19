4a1b15174150d1c829292efc46602eb0999c9be9
wzcxm <dark_zc@hotmail.com>
2018-07-19 18:42:27 +0800

    201807191842

--- Xiu_Game/app/Http/Controllers/UserManageController.php
+++ Xiu_Game/app/Http/Controllers/UserManageController.php
@@ -65,10 +65,62 @@ class UserManageController extends Controller
 
     public function getPlayer(Request $request){
         try{
+
             $uid = isset($request['uid'])?$request['uid']:0;
             $user = Users::find($uid);
             if(!empty($user)){
-                return response()->json(['player'=>$user]);
+                if(session("roleid")==4 && $user->super_aisle != session("aisle")){
+                    return response()->json(['error'=>'该玩家不在您名下！']);
+                }else if(session("roleid")==3 && $user->chief_aisle != session("aisle")){
+                    return response()->json(['error'=>'该玩家不在您名下！']);
+                }else{
+                    $ret_arr = [];
+                    $ret_arr["head_url"] = $user->head_img_url;
+                    $ret_arr["nick"] = $user->nickname;
+                    if($user->rid==4){
+                        $ret_arr["rname"] = "特级代理";
+                    }else if($user->rid==4){
+                        $ret_arr["rname"] = "总代";
+                    }else if($user->rid==6){
+                        $ret_arr["rname"] = "渠道代理";
+                    }else if($user->rid==2){
+                        $ret_arr["rname"] = "代理";
+                    }else{
+                        $ret_arr["rname"] = "玩家";
+                    }
+                    $ret_arr["front_uid"] = $user->front_uid;
+                    $ret_arr["chief_uid"] = $user->chief_uid;
+                    $ret_arr["create_time"] = $user->create_time;
+                    $tea_list = DB::select("select GROUP_CONCAT(tea_id) as tea_list from xx_sys_tea where uid=".$uid);
+                    $ret_arr["teaids"] =$tea_list[0]->tea_list;
+                    $start = date('Y-m-01');
+                    $end =  date('Y-m-t 23:59:59', strtotime($start));
+                    $all = DB::select('call search_grade_all('.$uid.')');
+                    $month = DB::select("call search_grade_month(".$uid.",'".$start."','".$end."')");
+                    $ret_arr["all"] =empty($all[0]->all_grade)?0:$all[0]->all_grade;
+                    $ret_arr["month"] = empty($month[0]->all_grade)?0:$month[0]->all_grade;
+                    if($user->rid==4){
+                        $count_play = DB::table('xx_user')->where('super_aisle',$user->aisle)->get();
+                        $ret_arr["count_agent"] = $count_play->where('rid','<>',5)->count();
+                        $ret_arr["count_player"] =$count_play->where('rid',5)->count();
+                    }else if($user->rid==3){
+                        $count_play = DB::table('xx_user')->where('chief_aisle',$user->aisle)->get();
+                        $ret_arr["count_agent"] = $count_play->where('rid','<>',5)->count();
+                        $ret_arr["count_player"] =$count_play->where('rid',5)->count();
+                    }else if($user->rid==6){
+                        $count_play = DB::table('xx_user')->where('vip_aisle',$user->aisle)->get();
+                        $ret_arr["count_agent"] = $count_play->where('rid','<>',5)->count();
+                        $ret_arr["count_player"] =$count_play->where('rid',5)->count();
+                    }else if($user->rid==2){
+                        $count_play = DB::table('xx_user')->where('front_uid',$uid)->orWhere('chief_uid',$uid)->get();
+                        $ret_arr["count_agent"] = $count_play->where('front_uid',$uid)->where('rid',2)->count();
+                        $ret_arr["count_player"] =$count_play->where('chief_uid',$uid)->where('rid',5)->count();
+                    }else{
+                        $ret_arr["count_agent"] = 0;
+                        $ret_arr["count_player"] =0;
+                    }
+                    return response()->json($ret_arr);
+                }
             }else {
                 return response()->json(['error'=>'玩家不存在！']);
             }
--- Xiu_Game/resources/views/PlayerSeach/playseachIndex.blade.php
+++ Xiu_Game/resources/views/PlayerSeach/playseachIndex.blade.php
@@ -46,13 +46,13 @@
 @endsection
 @section('easyui_content')
     <div  class="weui-flex head_bg" >
-        <div style="margin-top: 8%;">查询玩家</div>
+        <div style="margin-top: 8%;">资料查询</div>
     </div>
     <div class="weui-cells_form">
         <div class="weui-cell">
             <div style="width:100%;text-align: center;font-size: 0.8rem;">
                 <div style="float: left;width: 35%;text-align: right;">
-                    玩家ID：
+                    ID：
                 </div>
                 <div style="float: right;width: 60%;text-align: left;">
                     <input class="ipt_css" type="number" id="old_uid" style="width: 50%;">&nbsp;&nbsp;&nbsp;&nbsp;
@@ -63,18 +63,28 @@
         <div class="weui-cell">
             <div style="width:100%;text-align: center;font-size: 0.8rem;">
                 <div style="float: left;width: 35%;text-align: right;">
-                    玩家昵称：
+                    昵称：
                 </div>
                 <div style="float: right;width: 60%;text-align: left;">
                     <img  id="head_url" src="" style="border-radius:6px;" width="30" align="absmiddle" >
-                    <span style="color: #545454" id="player_nick"></span>
+                    <span style="color: #545454" id="nick"></span>
                 </div>
             </div>
         </div>
         <div class="weui-cell">
             <div style="width:100%;text-align: center;font-size: 0.8rem;">
                 <div style="float: left;width: 35%;text-align: right;">
-                    上级ID：
+                    级别：
+                </div>
+                <div style="float: right;width: 60%;text-align: left;">
+                    <span style="color: #545454" id="rname"></span>
+                </div>
+            </div>
+        </div>
+        <div class="weui-cell">
+            <div style="width:100%;text-align: center;font-size: 0.8rem;">
+                <div style="float: left;width: 35%;text-align: right;">
+                    填写上级ID：
                 </div>
                 <div style="float: right;width: 60%;text-align: left;">
                     <span style="color: #545454" id="front_uid"></span>
@@ -84,10 +94,70 @@
         <div class="weui-cell">
             <div style="width:100%;text-align: center;font-size: 0.8rem;">
                 <div style="float: left;width: 35%;text-align: right;">
-                    推荐人ID：
+                    邀请人ID：
                 </div>
                 <div style="float: right;width: 60%;text-align: left;">
                     <span style="color: #545454" id="chief_uid"></span>
+                </div>
+            </div>
+        </div>
+        <div class="weui-cell">
+            <div style="width:100%;text-align: center;font-size: 0.8rem;">
+                <div style="float: left;width: 35%;text-align: right;">
+                    注册日期：
+                </div>
+                <div style="float: right;width: 60%;text-align: left;">
+                    <span style="color: #545454" id="create_time"></span>
+                </div>
+            </div>
+        </div>
+        <div class="weui-cell">
+            <div style="width:100%;text-align: center;font-size: 0.8rem;">
+                <div style="float: left;width: 35%;text-align: right;">
+                    总业绩：
+                </div>
+                <div style="float: right;width: 60%;text-align: left;">
+                    <span style="color: #545454" id="all"></span>
+                </div>
+            </div>
+        </div>
+        <div class="weui-cell">
+            <div style="width:100%;text-align: center;font-size: 0.8rem;">
+                <div style="float: left;width: 35%;text-align: right;">
+                    当月业绩：
+                </div>
+                <div style="float: right;width: 60%;text-align: left;">
+                    <span style="color: #545454" id="month"></span>
+                </div>
+            </div>
+        </div>
+        <div class="weui-cell">
+            <div style="width:100%;text-align: center;font-size: 0.8rem;">
+                <div style="float: left;width: 35%;text-align: right;">
+                    旗下代理：
+                </div>
+                <div style="float: right;width: 60%;text-align: left;">
+                    <span style="color: #545454" id="count_agent"></span>
+                </div>
+            </div>
+        </div>
+        <div class="weui-cell">
+            <div style="width:100%;text-align: center;font-size: 0.8rem;">
+                <div style="float: left;width: 35%;text-align: right;">
+                    旗下玩家：
+                </div>
+                <div style="float: right;width: 60%;text-align: left;">
+                    <span style="color: #545454" id="count_player"></span>
+                </div>
+            </div>
+        </div>
+        <div class="weui-cell">
+            <div style="width:100%;text-align: center;font-size: 0.8rem;">
+                <div style="float: left;width: 35%;text-align: right;">
+                    名下牌馆：
+                </div>
+                <div style="float: right;width: 60%;text-align: left;">
+                    <span style="color: #545454" id="teaids"></span>
                 </div>
             </div>
         </div>
@@ -111,19 +181,31 @@
                     if(comm.is_null(data.error)){
                         $.alert(data.error);
                         $("#head_url").attr('src',"");
-                        $("#player_nick").html("");
+                        $("#nick").html("");
                         $("#front_uid").html("");
                         $("#chief_uid").html("");
+                        $("#create_time").html("");
+                        $("#teaids").html("");
+                        $("#rname").html("");
+                        $("#all").html("");
+                        $("#month").html("");
+                        $("#count_agent").html("");
+                        $("#count_player").html("");
                     } else {
-                        var player = data.player;
-                        console.log(comm.is_null(player['head_img_url']));
-                        if(comm.is_null(player['head_img_url'])){
-                            console.log(1);
-                            $("#head_url").attr('src',player['head_img_url']);
+                        var player = data;
+                        if(comm.is_null(player['head_url'])){
+                            $("#head_url").attr('src',player['head_url']);
                         }
-                        $("#player_nick").html(player['nickname']);
+                        $("#nick").html(player['nick']);
                         $("#front_uid").html(player['front_uid']);
                         $("#chief_uid").html(player['chief_uid']);
+                        $("#create_time").html(player['create_time']);
+                        $("#teaids").html(player['teaids']);
+                        $("#rname").html(player['rname']);
+                        $("#all").html(player['all']);
+                        $("#month").html(player['month']);
+                        $("#count_agent").html(player['count_agent']);
+                        $("#count_player").html(player['count_player']);
                     }
                 });
             });
