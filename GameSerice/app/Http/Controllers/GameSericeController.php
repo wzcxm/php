acecef817f385b23ae97fcf1fad126bb395bd9ac
wzcxm <dark_zc@hotmail.com>
2018-07-24 09:10:35 +0800

    201807240910

--- GameSerice/app/Http/Controllers/GameSericeController.php
+++ GameSerice/app/Http/Controllers/GameSericeController.php
@@ -777,11 +777,11 @@ EOT;
         if(empty($uid)) return "";
         if($role == 3){
             $sql = <<<EOT
-			select * from (select * from xx_sys_log where tea_id = $teaid  and uid = $uid ORDER BY create_date desc LIMIT 0,50) t ORDER BY t.create_date
+			select * from (select * from xx_sys_log where tea_id = $teaid and type <>4 and uid = $uid  ORDER BY create_date desc LIMIT 0,50) t ORDER BY t.create_date
 EOT;
         }else{
             $sql = <<<EOT
-			select * from (select * from xx_sys_log where tea_id = $teaid  and (uid = $uid or operate = $uid) ORDER BY create_date desc LIMIT 0,50) t ORDER BY t.create_date
+			select * from (select * from xx_sys_log where tea_id = $teaid and type <>4 and (uid = $uid or operate = $uid) ORDER BY create_date desc LIMIT 0,50) t ORDER BY t.create_date
 EOT;
         }
         $data =  DB::select($sql);
